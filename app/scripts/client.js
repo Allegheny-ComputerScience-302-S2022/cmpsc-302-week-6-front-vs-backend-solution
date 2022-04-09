6fc7d9fd149a8395fcc3bdeb22373c31c7100427
dluman <dluman@allegheny.edu>
2022-04-09 18:25:50 -0400

    Sending latest code to at least set up a websocket

--- app/scripts/client.js
+++ app/scripts/client.js
@@ -1,5 +1,9 @@
+var host = "wss://" + (location.host || "chat.cmpsc302.chompe.rs");
+
+console.log(host);
+
 var chat = {
   name: "Luman",
   msg: null,
-  socket: new WebSocket("ws://localhost:8080");
+  socket: new WebSocket(host)
 }
