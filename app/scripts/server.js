c98d838514558131b3303e743831f6b366d7444d
dluman <dluman@allegheny.edu>
2022-04-09 18:37:57 -0400

    Update with express static

--- app/scripts/server.js
+++ app/scripts/server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 const index = "index.html";
 
 const server = express()
-  .use(express.static(process.cwd())
+  .use(express.static(process.cwd()))
   .use((request, result) => result.sendFile(index, {root: process.cwd()}))
   .listen(port, () => console.log(`Listening on ${port}`));
 
