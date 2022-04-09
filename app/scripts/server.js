0783aec38f251a48cad0a712cc35391ee6a63e16
dluman <dluman@allegheny.edu>
2022-04-09 17:24:06 -0400

    Changing port to environment process var

--- app/scripts/server.js
+++ app/scripts/server.js
@@ -1,31 +1,33 @@
 const express = require('express');
 const ws = require('ws');
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 const index = "/index.html";
 
 const server = express()
   .use((request, result) => result.sendFile(index, {root: app}))
   .listen(port, () => console.log(`Listening on ${port}`));
 
+const socket = new Server({ server });
+
 var users = {};
 
-server.on("connection", (socket, request) => {
+socket.on("connection", (sock, request) => {
 
   let uid = 0;
 
   while(true) {
     if(!users.hasOwnProperty(uid)) {
-      users[uid] = socket;
+      users[uid] = sock;
       break;
     }
     uid++;
   }
 
-  socket.on("close", () => {
+  sock.on("close", () => {
     delete users[uid];
   });
 
-  socket.on("message", (message) => {
+  sock.on("message", (message) => {
     for (let user in users) {
       users[user].send(message.toString());
     }
