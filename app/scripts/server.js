b5c1a34eb319bf43a52a8dd4f822eaeeb61873b5
dluman <dluman@allegheny.edu>
2022-04-10 09:18:55 -0400

    String equality at server level to prevent ping messages from appearing

--- app/scripts/server.js
+++ app/scripts/server.js
@@ -14,7 +14,8 @@ var users = {};
 
 const emoji = {
   "greet": "👋",
-  "normal": "🗣️"
+  "normal": "🗣️",
+  "ping": "🔊",
 }
 
 socket.on("connection", (sock, request) => {
@@ -37,7 +38,7 @@ socket.on("connection", (sock, request) => {
     message = JSON.parse(message);
     let msg = message.text.toString();
     let type = emoji[message.type];
-    if (type == "ping") return false;
+    if (type === "ping") return false;
     for (let user in users) {
       users[user].send(`${type} ${msg}`);
     }
